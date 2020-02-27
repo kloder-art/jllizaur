9b6217604a64462496ba5ca7181c630489ce00b1
Javier López Úbeda <jlopezcur@gmail.com>
2020-02-27 12:28:43 +0100

    Using styled components on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,31 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'black',
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+const StyledHeader = styled.header`
+  margin-bottom: 1.45rem;
+  padding: 1.45rem 1.0875rem;
+  a {
+    color: rgba(0, 0, 0, 0.8);
+    text-decoration: none;
+  }
+`;
+
+const Header = ({ title }) => (
+  <StyledHeader>
+    <h1 style={{ margin: 0 }}>
+      <Link to="/">{title}</Link>
+    </h1>
+  </StyledHeader>
 );
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+  title: PropTypes.string,
 };
 
 Header.defaultProps = {
-  siteTitle: ``,
+  title: '',
 };
 
 export default Header;
