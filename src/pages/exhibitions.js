b6a43d4df1f886ad149e77e789489cd97eec3a63
Javier López Úbeda <jlopezcur@gmail.com>
2020-03-07 16:07:03 +0100

    Added exhibitions page

---
+++ src/pages/exhibitions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+import Container from '../components/Container';
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title={'CV'} />
+    <Container>En construcción.</Container>
+  </Layout>
+);
+
+export default ContactPage;
