170149cf5c8bada21c644ced5d683e9141302513
jlopezcur <jlopezcur@users.noreply.github.com>
2020-03-09 12:29:01 +0000

    deploy: 4d0db5a7df7dae76285a1545373179784716ce35

--- sw.js
+++ sw.js
@@ -27,36 +27,28 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-278e0675744fd585b4b2.js"
+    "url": "webpack-runtime-254a38171cc836116684.js"
   },
   {
     "url": "commons-8c8055fdcffe9c3c4d24.js"
   },
   {
-    "url": "app-8f0130661481257d61cb.js"
+    "url": "app-506106cbe7cb175d84cf.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-2fb560e943ea9fad2129.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a585863d7f8ba4ded8465b2adfb7462e"
+    "revision": "9c8070652cd14a6e2444635216bffac7"
   },
   {
     "url": "google-fonts/s/lato/v16/S6uyw4BMUTPHjx4wXg.woff2",
     "revision": "b4d2c4c39853ee244272c04999b230ba"
   },
   {
-    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "c355c8040c47a63bfb3360e4b7cb6553"
-  },
-  {
-    "url": "page-data/app-data.json",
-    "revision": "96d9b76c6bc285d838a7f0ae212a34b6"
-  },
-  {
     "url": "manifest.webmanifest",
-    "revision": "2f1370e146d7de6189f7673859b8d9bb"
+    "revision": "73e852f44577b983b583bbd37cdcb57c"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -144,12 +136,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   lastNavigationRequest = event.request.url
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^/jlizaur`), ``)
+  pathname = pathname.replace(new RegExp(`^`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/jlizaur/app-8f0130661481257d61cb.js`))) {
+  if (!resources || !(await caches.match(`/app-506106cbe7cb175d84cf.js`))) {
     return await fetch(event.request)
   }
 
@@ -162,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/jlizaur/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
